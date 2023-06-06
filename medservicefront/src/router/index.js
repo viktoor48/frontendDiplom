ee967d5b79414c99d31780d17163f115a81eea25
viktoor48 <71779943+viktoor48@users.noreply.github.com>
2023-06-06 17:08:00 +0300

    authorization

--- medservicefront/src/router/index.js
+++ medservicefront/src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import {UserRoles} from "@/constants/constants";
 import MainPage from "@/components/MainPage";
 import RegistrationPatientPage from "@/components/RegistrationPatientPage";
 import RegistrationDoctorPage from "@/components/RegistrationDoctorPage";
@@ -14,12 +15,48 @@ import ClientProfile from "@/components/ClientProfile";
 import DoctorProfile from "@/components/DoctorProfile";
 import ClinicProfile from "@/components/ClinicProfile";
 
+
+/**
+ *
+ * @type {boolean}
+ */
+
+// eslint-disable-next-line no-unused-vars
+const authGuard = function (to, from, next) {
+  const isAuthorized = Object.prototype.hasOwnProperty.call(localStorage, 'token');
+  console.log(isAuthorized);
+  console.log(localStorage.getItem('token'));
+  if (!isAuthorized) next({name: 'home'});
+  else next();
+}
+
+const clientAuthGuard = function (to, from, next) {
+  const isAuthorized = Object.prototype.hasOwnProperty.call(localStorage, 'token');
+  if (!isAuthorized) next({name: 'home'});
+  else if (localStorage.getItem('userRole') !== UserRoles.Client) next({name: 'home'});
+  else next();
+}
+
+const clinicAuthGuard = function (to, from, next) {
+  const isAuthorized = Object.prototype.hasOwnProperty.call(localStorage, 'token');
+  if (!isAuthorized) next({name: 'home'});
+  else if (localStorage.getItem('userRole') !== UserRoles.Clinic) next({name: 'home'});
+  else next();
+}
+
+const doctorAuthGuard = function (to, from, next) {
+  const isAuthorized = Object.prototype.hasOwnProperty.call(localStorage, 'token');
+  if (!isAuthorized) next({name: 'home'});
+  else if (localStorage.getItem('userRole') !== UserRoles.Doctor) next({name: 'home'});
+  else next();
+}
+
 const routes = [
   {
     path: '/',
     name: 'home',
     component: MainPage,
-    alias: '/'
+    alias: '/',
   },
   {
     path: '/registrationPatient',
@@ -76,16 +113,19 @@ const routes = [
     path: '/clientProfile',
     name: 'clientProfile',
     component: ClientProfile,
+    beforeEnter: clientAuthGuard,
   },
   {
     path: '/clinicProfile',
     name: 'clinicProfile',
     component: ClinicProfile,
+    beforeEnter: clinicAuthGuard,
   },
   {
     path: '/doctorProfile',
     name: 'doctorProfile',
     component: DoctorProfile,
+    beforeEnter: doctorAuthGuard,
   },
 ]
 
